43c772de31d77e75e4813995494b4dbfe704c1aa
ThomasCast3 <91255859+ThomasCast3@users.noreply.github.com>
2021-12-16 16:16:08 +0100

    incroyable 1fois/10

--- src/main/java/com/tgrm/labyrtintheteam_tgrm/models/labyrinthe.java
+++ src/main/java/com/tgrm/labyrtintheteam_tgrm/models/labyrinthe.java
@@ -21,13 +21,17 @@ public class labyrinthe {
            }
        }
 
-       List<MazeCell> path = generator(labyrinthe, height,width);
+       List<List<MazeCell>> pathlist = generator( height,width,0,0);
+       for (int l=0; l<pathlist.size();l++){
+           List<MazeCell>  path = pathlist.get(l);
+
 
         for(int i = 0; i< path.size()-1;i++){
           int posX1 = path.get(i).getX();
           int posY1 = path.get(i).getY();
           int posX2 = path.get(i+1).getX();
           int posY2 = path.get(i+1).getY();
+
 
           if(posX1<posX2 && labyrinthe[posX1][posY1]==0){   //aller a droite et la case actuel est pleine
               labyrinthe[posX1][posY1]=1;  // on casse le mur de droite sur la case actuel
@@ -100,16 +104,20 @@ public class labyrinthe {
            System.out.println("");
 
 
-     }
+            }
+        }
     }
 
-     static List<MazeCell> generator(int [][]tab, int height, int width) {
+     static List<List<MazeCell>> generator( int height, int width,int x,int y ) {
          boolean fin = false;
-         int posX = 0;
-         int posY = 0;
+         boolean caseVide = false;
+         int posX = x;
+         int posY = y;
          List<MazeCell> nextCell = new ArrayList();
          List<MazeCell> path = new ArrayList();
          List<MazeCell> voisins = new ArrayList<>();
+         List<List<MazeCell>> pathList = new ArrayList();
+
          boolean [][] visited = new boolean[width][height];
 
 
@@ -126,63 +134,73 @@ public class labyrinthe {
          visited[0][0] = true;
          path.add(new MazeCell(0,0));
 
-        while (!fin) {
-            nextCell.removeAll(nextCell);
-            boolean voisinX = false;
-            boolean voisinY = false;
-            xmap.put(0 , posX+1);
-            xmap.put(width-1 , posX-1);
-            ymap.put(0 , posY+1);
-            ymap.put(height-1 , posY-1);
-             if (posX == 0 || posX == width -1) {
-                     if (!visited[xmap.get(posX)][posY]){
-                         nextCell.add(new MazeCell(xmap.get(posX),posY));
-                         voisinX  = true;
-                 }
-             }
-             else{
-                 for (int i=-1;i<=1;i+=2){
-                     if (!visited[posX + i][posY]) {
-                         nextCell.add(new MazeCell(posX+i, posY));
-                         voisinX = true;
-                     }
-                 }
-             }
-             if (posY == 0) {
-                 if (!visited[posX][posY + 1]){
-                     nextCell.add(new MazeCell(posX,posY+1));
-                     voisinY = true;
-                 }
-             }
-             else if (posY==height-1) {
-                 if (!visited[posX][posY - 1]){
-                     nextCell.add(new MazeCell(posX,posY-1));
-                     voisinY = true;
-                 }
-             }
-             else{
-                 for (int i=-1;i<=1;i+=2){
-                     if (!visited[posX][posY + i]) {
-                         nextCell.add(new MazeCell(posX, posY + i));
-                         voisinY = true;
-                     }
-                 }
-             }
+         for (int cols=0;cols<height;cols++){
+             for(int raw = 0;raw<width;raw++ ){
+                 while(!visited[cols][raw]){
 
-            if (!voisinX && !voisinY){
-                fin = true;
-                break;
-            }
 
-            int taille = nextCell.size();
-            long  result =  Math.round((Math.random() * (taille-1)));
+                     while (!fin) {
+                         nextCell.removeAll(nextCell);
 
-            posX = nextCell.get((int)result).getX();
-            posY = nextCell.get((int)result).getY();
-            visited[posX][posY] = true;
-            path.add(new MazeCell(posX,posY));
+                         boolean voisinX = false;
+                         boolean voisinY = false;
+                         xmap.put(0, posX + 1);
+                         xmap.put(width - 1, posX - 1);
+                         ymap.put(0, posY + 1);
+                         ymap.put(height - 1, posY - 1);
+                         if (posX == 0 || posX == width - 1) {
+                             if (!visited[xmap.get(posX)][posY]) {
+                                 nextCell.add(new MazeCell(xmap.get(posX), posY));
+                                 voisinX = true;
+                             }
+                         } else {
+                             for (int i = -1; i <= 1; i += 2) {
+                                 if (!visited[posX + i][posY]) {
+                                     nextCell.add(new MazeCell(posX + i, posY));
+                                     voisinX = true;
+                                 }
+                             }
+                         }
+                         if (posY == 0) {
+                             if (!visited[posX][posY + 1]) {
+                                 nextCell.add(new MazeCell(posX, posY + 1));
+                                 voisinY = true;
+                             }
+                         } else if (posY == height - 1) {
+                             if (!visited[posX][posY - 1]) {
+                                 nextCell.add(new MazeCell(posX, posY - 1));
+                                 voisinY = true;
+                             }
+                         } else {
+                             for (int i = -1; i <= 1; i += 2) {
+                                 if (!visited[posX][posY + i]) {
+                                     nextCell.add(new MazeCell(posX, posY + i));
+                                     voisinY = true;
+                                 }
+                             }
+                         }
 
-          for (int y=0;y< voisins.size();y++) {
+                         if (!voisinX && !voisinY) {
+                             fin = true;
+                             break;
+                         }
+
+                         int taille = nextCell.size();
+                         long result = Math.round((Math.random() * (taille - 1)));
+
+                         posX = nextCell.get((int) result).getX();
+                         posY = nextCell.get((int) result).getY();
+                         visited[posX][posY] = true;
+                         path.add(new MazeCell(posX, posY));
+
+                         nextCell.remove((int) result);
+                         for (int i = 0; i < nextCell.size(); i++) {
+                             if (!voisins.contains(nextCell.get(i))) {
+                                 voisins.add(nextCell.get(i));
+                             }
+                         }
+
+      /*    for (int y=0;y< voisins.size();y++) {
               System.out.println(nextCell.get((int)result));
               System.out.println(voisins.get(y));
              if(nextCell.get((int)result).equals(voisins.get(y))){
@@ -190,16 +208,32 @@ public class labyrinthe {
                    voisins.remove(voisins.get(y));
                }
            }
-            nextCell.remove((int)result);
-            for(int i=0;i< nextCell.size();i++) {
-                if(!voisins.contains(nextCell.get(i))) {
-                    voisins.add(nextCell.get(i));
-                }
-            }
+                   */
 
-         }
-         System.out.println(voisins);
-        return path;
+                     }
+                     for(int l=0; l<voisins.size();l++){
+                         if(!visited[voisins.get(l).getX()][voisins.get(l).getY()]){
+                             posX = voisins.get(l).getX();
+                             posY = voisins.get(l).getY();
+                             fin = false;
+                             visited[posX][posY]=true;
+                             path.add(new MazeCell(posX,posY));
+                            
+                         }
+                     }
+
+                     pathList.add(path);
+
+               }
+           }
+       }
+
+      /* if (caseVide){
+           generator(height,width,x,y);
+
+       }*/
+         return pathList;
+
 
      }
 }
